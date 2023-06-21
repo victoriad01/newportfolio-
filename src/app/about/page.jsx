5a34b3ef72ee9ce9d0f299a5b9e273ce3c816d0f
victoriad01 <94958284+victoriad01@users.noreply.github.com>
2023-06-21 18:55:01 +0100

    added br to about me

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -29,11 +29,16 @@ const About = () => {
           <p className={styles.desc}>
             I am Victor Iyanu Oluwafemi. A Graduate and MSC holder from the
             prestigious Federal University of Technology Akure. I have over
-            three years of experience with web applications. I have experience
-            coding React, React Native, NextJS, HTML, CSS etc. Also, I am vast
-            in Node and ExpressJS. In the past I have built CRM and ERP
-            solutions for companies to improve their business processes. Samples
-            of this can be found <Link href='/portifolio' className={styles.here}>here</Link>.
+            three years of experience with web applications.
+            <br />
+            <br />I have experience coding React, React Native, NextJS, HTML,
+            CSS etc. Also, I am vast in Node and ExpressJS. In the past I have
+            built CRM and ERP solutions for companies to improve their business
+            processes. Samples of this can be found
+            <Link href='/portifolio' className={styles.here}>
+              here
+            </Link>
+            .
           </p>
         </div>
         <div className={styles.item}>
@@ -44,42 +49,55 @@ const About = () => {
             background in Graphics Design and UI/UX. I have expertise in React,
             React Native, NextJS Node.js, and Express.js, and I am adept at
             crafting intuitive and responsive user interfaces that engage and
-            delight users. Having transitioned from Graphics Design and UI/UX, I
-            bring a unique perspective to web and mobile development. I combine
-            my design sensibilities with technical skills to create visually
-            appealing and user-friendly applications that seamlessly blend
-            aesthetics and functionality. My technical skills include: Frontend
-            Development: Proficient in React and React Native, I leverage these
-            frameworks to bring designs to life and create visually stunning and
-            interactive user interfaces for web applications and mobile
-            applications.
+            delight users.
+            <br />
+            <br />
+            Having transitioned from Graphics Design and UI/UX, I bring a unique
+            perspective to web and mobile development. I combine my design
+            sensibilities with technical skills to create visually appealing and
+            user-friendly applications that seamlessly blend aesthetics and
+            functionality.
+            <br />
+            <br />
+            My technical skills include: Frontend Development: Proficient in
+            React and React Native, I leverage these frameworks to bring designs
+            to life and create visually stunning and interactive user interfaces
+            for web applications and mobile applications.
             <br />
             <br />
             Backend Development: Experienced in building robust and scalable
             server-side applications using Node.js and Express.js, I ensure
             smooth data flow and efficient communication between the frontend
-            and backend. Testing: Skilled in End-to-End Testing and Unit
-            Testing, I prioritize code quality and maintainability, implementing
+            and backend.
+            <br />
+            <br />
+            Testing: Skilled in End-to-End Testing and Unit Testing, I
+            prioritize code quality and maintainability, implementing
             comprehensive test suites to ensure software reliability and
-            minimize bugs. Continuous Integration and Continuous Deployment
-            (CI/CD): Proficient in setting up CI/CD pipelines, I automate the
-            build, test, and deployment processes to facilitate efficient and
-            seamless software delivery. Throughout my career, I have
-            successfully delivered complex projects, collaborating closely with
-            cross-functional teams to meet project requirements and
-            deliverables. I am adept at translating design concepts into
-            functional code and implementing innovative solutions to overcome
-            challenges.
+            minimize bugs.
+            <br />
+            <br />
+            Continuous Integration and Continuous Deployment (CI/CD): Proficient
+            in setting up CI/CD pipelines, I automate the build, test, and
+            deployment processes to facilitate efficient and seamless software
+            delivery. Throughout my career, I have successfully delivered
+            complex projects, collaborating closely with cross-functional teams
+            to meet project requirements and deliverables. I am adept at
+            translating design concepts into functional code and implementing
+            innovative solutions to overcome challenges.
             <br />
             <br />
             As a lifelong learner, I constantly seek to expand my knowledge and
             stay updated with the latest technologies and industry best
             practices. I thrive in fast-paced environments that foster
-            creativity, collaboration, and continuous improvement. If you are
-            looking for a dedicated and results-driven Full Stack Engineer with
-            a frontend focus and proficiency in React, React Native, NextJS
-            Node.js, Express.js, End-to-End Testing, Unit Testing, and CI/CD.
-            Kindly use the contact me session to send an email.
+            creativity, collaboration, and continuous improvement.
+            <br />
+            <br />
+            If you are looking for a dedicated and results-driven Full Stack
+            Engineer with a frontend focus and proficiency in React, React
+            Native, NextJS Node.js, Express.js, End-to-End Testing, Unit
+            Testing, and CI/CD. Kindly use the contact me session to send an
+            email.
           </p>
           <Button url='/contact' text='Contact me!' />
         </div>
